f8ddfca3c71bffd4960eac88af56b6285f65daca
Konstantin Semenov and Luis Urraca <pair+konstantin+luis@pivotallabs.com>
2016-01-26 16:55:19 +0000

    [#112426455] Test framework setup

---
+++ Podfile
@@ -0,0 +1,14 @@
+use_frameworks!
+
+def testing_pods
+  pod 'Quick', '~> 0.9'
+  pod 'Nimble', '~> 3.1'
+end
+
+target 'SettlerTests' do
+  testing_pods
+end
+
+target 'SettlerUITests' do
+  testing_pods
+end
--- SettlerTests/SettlerTests.swift
+++ SettlerTests/SettlerTests.swift
@@ -1,36 +1,12 @@
-//
-//  SettlerTests.swift
-//  SettlerTests
-//
-//  Created by Pivotal on 26/01/2016.
-//  Copyright (c) 2016 Pivotal Labs. All rights reserved.
-//
+import Quick
+import Nimble
 
-import XCTest
-@testable import Settler
-
-class SettlerTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
+class SettlerSpec: QuickSpec {
+    override func spec() {
+        describe("Basic test") {
+            it("should be true") {
+                expect(true).to(beTruthy())
+            }
         }
     }
-    
 }
